52fa3e1f0c6f8a437061b6bd77b61a4424134775
985616770 <985616770@qq.com>
2018-07-16 21:47:43 +0800

    1

--- 高级程序设计代码/第八章BOM.js
+++ 高级程序设计代码/第八章BOM.js
@@ -1,6 +1,22 @@
 /*
  * @Author: xuanjidd 
- * @Date: 2018-07-15 09:16:40 
- * @Last Modified by:   xaunjidd 
- * @Last Modified time: 2018-07-15 09:16:40 
+ * @Date: 2018-07-15 15:47:30 
+ * @Last Modified by: xaunjidd
+ * @Last Modified time: 2018-07-16 15:54:05
+ * 
+ * 高级程序设计第八章
  */
+
+//8.1.1 window对象和全局作用域
+// var age = 29;
+
+// function sayAge() {
+//     console.log(this.age);
+// }
+
+// console.log(age);
+// sayAge();
+// Window.sayAge();
+
+//8.1.2窗口关系及框架
+
