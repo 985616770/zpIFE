2565aa81f39c38c47d07db6c42e2e26962381470
985616770 <985616770@qq.com>
2018-07-24 20:37:28 +0800

    1

---
+++ 高级程序设计代码/第十一章选择符API.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: xuanjidd 
+ * @Date: 2018-07-24 15:06:07 
+ * @Last Modified by: xaunjidd
+ * @Last Modified time: 2018-07-24 15:37:32
+ * 
+ * 第十一章  DOM拓展
+ */
+
+ //11.1.1querySelector()方法
+//body 元素
+ var body = document.querySelector("body");
+//id 为#mydiv
+ var myDiv = document.querySelector("#myDiv");
+
+ //取得类为"selected"第一个元素
+ var selected = document.querySelector(".selected");
+
+ //取得类为"button"的第一个图像元素
+
+ var img = document.body.querySelector("img.button");
+
+
+ //11.1.2 querySelectorAll()方法
+
+ 
