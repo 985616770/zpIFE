d4258dcef20cd45b4488ae4f7456372d3457c48f
zhangpeng <985616770@qq.com>
2019-01-16 15:33:44 +0800

    1

--- 2018/31-33/index.js
+++ 2018/31-33/index.js
@@ -1,135 +1,161 @@
-const selected = document.getElementById('region-select');
-const selected2 = document.getElementById('region-select2');
-const table = document.getElementById('table-wrapper');
-const reAll = document.getElementById('regionAll');
-const proAll = document.getElementById('productAll');
-const container = [];
-
-var status = 0;
-var status2 = 0;
-
-function adjust(status, selected) {
-  if (status == 0) {
-    for (let i = 0; i < selected.children.length; i++) {
-      const el = selected.children[i];
-      el.checked = true;
+// 筛选关键词
+function spliceValue(key, obj, filter) {
+  var i = 0;
+  var result = [];
+  var arr = [];
+  obj.forEach(e => {
+    if (!arr.includes(e[key])) {
+      arr.push(e[key]);
+      result.push({
+        value: ++i,
+        text: e[key]
+      });
     }
-    status = 1;
-    return status;
-  } else if (status == 1) {
-    for (let i = 0; i < selected.children.length; i++) {
-      const el = selected.children[i];
-      el.checked = false;
-    }
-    status = 0;
-    return status;
+  });
+  if (filter) {
+    return arr;
   }
+  return result;
 }
 
-console.log(adjust(status, selected));
-console.log(adjust(status, selected));
-console.log(status);
-
-reAll.onclick = function() {
-  if (status === 0) {
-    for (let i = 0; i < selected.children.length; i++) {
-      const el = selected.children[i];
-      el.checked = true;
-    }
-
-    status = 1;
-  } else if (status === 1) {
-    for (let i = 0; i < selected.children.length; i++) {
-      const el = selected.children[i];
-      el.checked = false;
-    }
-    status = 0;
-  }
+_filter = {
+  product: spliceValue('product', sourceData, 'all'),
+  region: spliceValue('region', sourceData, 'all')
 };
-proAll.onclick = function() {
-  if (status2 === 0) {
-    for (let i = 0; i < selected2.children.length; i++) {
-      const el = selected2.children[i];
-      el.checked = true;
-    }
-    status2 = 1;
-  } else if (status2 === 1) {
-    for (let i = 0; i < selected2.children.length; i++) {
-      const el = selected2.children[i];
-      el.checked = false;
-    }
-    status2 = 0;
-  }
-};
-selected.onchange = function(e) {
-  const input = e.target;
-  if (input.checked) {
-    container.push(input);
-  } else {
-    container.pop(input);
-  }
-  if (container.length == 3) {
-    for (let i = 0; i < selected.children.length; i++) {
-      const el = selected.children[i];
-      el.checked = true;
-    }
-    reAll.checked = true;
-  } else if (container.length < 3) {
-    reAll.checked = false;
-  }
-  console.log(container);
-};
+console.log(_filter);
 
-selected2.onchange = function(e) {
-  var data = getData();
-  render(data);
-};
+function returnTable(filter) {
+  return `<table style='width:100%;text-align:center;'>
+                            <tr>
+                                <th>商品</th>
+                                <th>地区</th>
+                                <th>1月</th>
+                                <th>2月</th>
+                                <th>3月</th>
+                                <th>4月</th>
+                                <th>5月</th>
+                                <th>6月</th>
+                                <th>7月</th>
+                                <th>8月</th>
+                                <th>9月</th>
+                                <th>10月</th>
+                                <th>11月</th>
+                                <th>12月</th>
+                            </tr>
+                            ${returnTbody(filter)}
+                            
+                    </table>`;
 
-function getData() {
-  const value = selected.value;
-  const value2 = selected2.value;
-  const newTable = [];
-  for (let i = 0; i < sourceData.length; i++) {
-    const element = sourceData[i];
-    if (element.region == value && element.product == value2) {
-      newTable.push(element);
-    }
+  function returnTbody(filter) {
+    var filter = filter || _filter;
+    var long = [filter.product.length, filter.region.length];
+    var txt = '';
+    sourceData.forEach((e, i) => {
+      if (
+        filter.product.includes(e.product) &&
+        filter.region.includes(e.region)
+      ) {
+        txt += '<tr>';
+
+        if (long[0] <= 1 && i == 0) {
+          txt += '<td rowspan=' + long[1] + '>';
+          txt += e.product;
+          txt += '</td>';
+        } else if (long[0] > 1) {
+          txt += '<td>';
+          txt += e.product;
+          txt += '</td>';
+        }
+
+        if (long[1] <= 1 && i == 0) {
+          txt += '<td rowspan=' + long[0] + '>';
+          txt += e.region;
+          txt += '</td>';
+        } else if (long[1] > 1) {
+          txt += '<td>';
+          txt += e.region;
+          txt += '</td>';
+        }
+
+        e.sale.forEach(e => {
+          txt += '<td>';
+          txt += e;
+          txt += '</td>';
+        });
+        txt += '</tr>';
+      }
+    });
+    return txt;
   }
-  return newTable;
 }
+document.querySelector('#result').innerHTML = returnTable();
 
-function render(data) {
-  table.innerHTML = `<table border = '1' id="table">
-                          <thead>
-                          <tr><th>商品</th>
-                          <th>地区</th>
-                          <th>1月</th>
-                          <th>2月</th>
-                          <th>3月</th>
-                          <th>4月</th>
-                          <th>5月</th>
-                          <th>6月</th>
-                          <th>7月</th>
-                          <th>8月</th>
-                          <th>9月</th>
-                          <th>10月</th>
-                          <th>11月</th>
-                          <th>12月</th>
-                        </tr>
-`;
-  for (let i = 0; i < data.length; i++) {
-    const ele = data[i];
-    table.children[0].innerHTML += `<tr>
-                          <td>${ele.product}</td>
-                          <td>${ele.region}</td>
-                        </tr>`;
-    console.dir(table.children[0].children[i].children[0]);
-    for (let j = 0; j < ele.sale.length; j++) {
-      const num = ele.sale[j];
-      table.children[0].children[i + 1].children[0].innerHTML += `
-      <td>${num}</td>
-      `;
+// 生成选择
+function CheckBox(dom, obj, cof) {
+  var el =
+    typeof dom === 'string' &&
+    !!document.querySelector(dom) &&
+    document.querySelector(dom);
+  if (!el) return;
+  var text = cof.text || '';
+  var name = cof.name || '';
+  el.innerHTML += `
+            <label>全部${text}<input type="checkbox" name='${name}' checked data-type='all'></label>
+            ${obj
+              .map(element => {
+                return (
+                  '<label>' +
+                  element.text +
+                  '<input type="checkbox" checked name="' +
+                  name +
+                  '" data-type="' +
+                  element.value +
+                  '" data-text="' +
+                  element.text +
+                  '"></label>'
+                );
+              })
+              .join('')}
+            `;
+  var all = el.querySelector('[data-type="all"]');
+  var ck = el.querySelectorAll('input:not([data-type="all"])');
+  var checkArr = {};
+  el.addEventListener('change', function(e) {
+    var e = e || window.event;
+    var t = e.target;
+    var haschecked = Array.prototype.some.call(ck, e => {
+      return e.checked == 1;
+    });
+    var allChecked = Array.prototype.every.call(ck, e => {
+      return e.checked == 1;
+    });
+    if (t === all) {
+      ck.forEach(e => {
+        e.checked = t.checked;
+        _filter[t.name] = spliceValue(t.name, sourceData, 'all');
+      });
+      if (t.checked == false) {
+        ck[0].checked = 1;
+        _filter[t.name] = [spliceValue(t.name, sourceData, 'all')[0]];
+      }
+    } else {
+      if (!haschecked) {
+        return false;
+        t.checked = 1;
+      }
+      t.checked
+        ? _filter[t.name].splice(t.dataset.type - 1, 0, t.dataset.text)
+        : _filter[t.name].splice(t.dataset.type - 1, 1);
+      all.checked = allChecked;
     }
-  }
-  table.children[0].innerHTML += `</table>`;
+    document.querySelector('#result').innerHTML = returnTable();
+  });
 }
+CheckBox('#region-radio-wrapper', spliceValue('region', sourceData), {
+  text: '地区',
+  name: 'region'
+});
+CheckBox('#product-radio-wrapper', spliceValue('product', sourceData), {
+  text: '种类',
+  name: 'product'
+});
